d1704514e1cdcceb80e0eadf269f052f624f7e08
ErikMac <erikyuyz@gmail.com>
2019-03-03 02:54:40 +0800

    dev

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -9,13 +9,6 @@ export class HeaderComponent {
   isHeaderTitleString: boolean;
   private _headerTitle: string | TemplateRef<void>;
 
-  tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-
   @Input()
   get headerTitle(): string | TemplateRef<void> {
     return this._headerTitle;

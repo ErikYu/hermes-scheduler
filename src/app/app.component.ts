31d4360f1d922b1d6167db23da50a7095483dbb5
ErikMac <erikyuyz@gmail.com>
2019-03-07 18:46:02 +0800

    add sign out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
+  providers: [LoginService],
 })
 export class AppComponent {
   title = 'hermes-scheduler';
@@ -11,4 +13,12 @@ export class AppComponent {
   menu = [
 
   ];
+
+  constructor(
+    private _login: LoginService,
+  ) {}
+
+  signOut() {
+    this._login.signOut().subscribe(res => {});
+  }
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {finalize} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class LoginService {
   constructor(
     private http: HttpClient,
+    private _router: Router,
   ) {}
 
   login(payload: {login: string, password: string}): Observable<any> {
     return this.http.post('/api/login', payload);
+  }
+
+  signOut() {
+    return this.http.get('/api/login/out').pipe(
+      finalize(() => {
+        this._router.navigate(['/login']);
+      })
+    );
   }
 }
